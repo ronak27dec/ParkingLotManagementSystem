1b61c86e05311b0231dc3d09ca68927942fba539
ronak <rmal0008@synchronoss.com>
2024-02-03 17:35:48 +0530

    Added workspace and project files

---
+++ ParkingLot/ParkingLot/ParkingLotApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkingLotApp.swift
+//  ParkingLot
+//
+//  Created by Ronak Malick on 03/02/24.
+//
+
+import SwiftUI
+
+@main
+struct ParkingLotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
